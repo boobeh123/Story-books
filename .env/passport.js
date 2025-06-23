5ff3b00a3569f4e4ff0509a7df5c48a519e3978e
Bobby <boobeh321@gmail.com>
2025-06-22 15:25:46 -1000

    revert callbackURL - todo

--- .env/passport.js
+++ .env/passport.js
@@ -6,7 +6,8 @@ module.exports = function(passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "https://bobstorybook.up.railway.app/auth/google/callback"        
+        callbackURL: "https://boobeh-storybooks.onrender.com/auth/google/callback"
+        
     }, 
     async (accessToken, refreshToken, profile, done) => {
         const newUser = {
